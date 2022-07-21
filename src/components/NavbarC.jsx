dd4d1bfa07e0e93fe5c931ababc28ee90de33e25
Santanu Mohapatra <99542723+Santanu55555@users.noreply.github.com>
2022-07-21 18:27:42 +0530

    checkout navbar

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -2,10 +2,10 @@ import { Box, Slider, SliderFilledTrack, SliderMark, SliderThumb, SliderTrack, T
 import React from 'react'
 
 function NavbarC() {
-  const [sliderValue, setSliderValue] = React.useState(5)
-  const [showTooltip, setShowTooltip] = React.useState(false)
+  const [sliderValue, setSliderValue] = React.useState(20)
+ 
   return (
-    <Box padding="0px 300px 0px 300px" filter="grayscale(100%)">
+    <Box padding="0px 300px 50px 300px" filter="grayscale(100%)">
       <Slider
       id='slider'
       defaultValue={25}
@@ -13,33 +13,32 @@ function NavbarC() {
       max={100}
       colorScheme='teal'
       onChange={(v) => setSliderValue(v)}
-      onMouseEnter={() => setShowTooltip(true)}
-      onMouseLeave={() => setShowTooltip(false)}
+   
     >
-      <SliderMark value={20} mb='1' ml='2.5' fontSize='sm'>
-        25%
+      <SliderMark value={20} mb='10' ml='2.5' fontSize='sm'>
+      SHOPPING BAG
       </SliderMark>
       <SliderMark value={40} mt='1' ml='-2.5' fontSize='sm'>
-        50%
+      SELECT ADDRESS
       </SliderMark>
       <SliderMark value={60} mt='1' ml='-2.5' fontSize='sm'>
-        75%
+      CHECKOUT
       </SliderMark>
       <SliderMark value={80} mt='1' ml='-2.5' fontSize='sm'>
-        100%
+      PAYMENT
       </SliderMark>
       <SliderTrack>
-        <SliderFilledTrack />
+        <SliderFilledTrack bg='black'/>
       </SliderTrack>
       <Tooltip
         hasArrow
         bg='teal.500'
         color='white'
         placement='top'
-        isOpen={showTooltip}
-        label={`${sliderValue}%`}
+        
+       
       >
-        <SliderThumb />
+        <SliderThumb borderRadius='50%' border='5px solid lightgray'/>
       </Tooltip>
     </Slider>
     </Box>
