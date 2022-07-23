f6172d01ab4f6f01a0dad7767f60a75f4e6e35c3
Santanu Mohapatra <99542723+Santanu55555@users.noreply.github.com>
2022-07-23 16:02:05 +0530

    address fully done

--- src/pages/AddressCard.jsx
+++ src/pages/AddressCard.jsx
@@ -2,8 +2,10 @@ import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 import { AiOutlineMail } from 'react-icons/ai'
 import { BiPhoneCall } from 'react-icons/bi'
+import { useNavigate } from 'react-router-dom'
 import './CheckOut.css'
 export const AddressCard = ({name,mobile, email, type,add, pincode,city,state}) => {
+    const navigate=useNavigate()
   return (
     <Box className='mehudon shahrukh' lineHeight='10' fontSize={15} style={{width:"250px",height:"300px",fontFamily:'Monospace,Monaco'}} >
         <Text fontWeight="600">{type}</Text>
@@ -16,7 +18,7 @@ export const AddressCard = ({name,mobile, email, type,add, pincode,city,state}) 
         </Flex>
        <Flex justifyContent="center"><Box allign='center' mt='3'><BiPhoneCall/></Box><Text allign='center' display='flex'>{mobile} </Text></Flex>
         <Flex justifyContent="center"> <Box mt='3'><AiOutlineMail/></Box><Text allign='center'>{email}</Text></Flex>
-    <Button align='bottom' bg='black' color='white' mt='2' >Select</Button>
+    <Button align='bottom' bg='black' color='white' mt='2' onClick={()=>navigate("/checkout")} >Select</Button>
     </Box>
   )
 }
